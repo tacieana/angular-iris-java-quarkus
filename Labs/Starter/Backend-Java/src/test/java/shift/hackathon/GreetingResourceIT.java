aa969e04d7bc88dbd3a742b8d4e17610fcd9e826
Cesar Cruz <cesar.cruz@shift.com.br>
2022-10-08 14:48:07 -0300

    starter template backend java

---
+++ Labs/Starter/Backend-Java/src/test/java/shift/hackathon/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package shift.hackathon;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
